fa1c210c06bc81dfedc3845c30ddbc054551f993
art rimbaud <rimbaudcode@gmail.com>
2016-10-24 23:56:10 +0200

    code refactored

--- emacs.d/rmbd/shell-region.el
+++ emacs.d/rmbd/shell-region.el
@@ -1,7 +1,5 @@
 (defun shell-region (start end)
   "execute region in an inferior shell"
   (interactive "r")
-  (shell-command
-   (buffer-substring-no-properties start end)
-   )
+  (shell-command (buffer-substring-no-properties start end))
   )
