55460939c58d18cd6f69b9a3fe3211ad3ef31e9c
art rimbaud <rimbaudcode@gmail.com>
2016-10-24 23:54:04 +0200

    code refactored

--- emacs.d/rmbd/hlint.el
+++ emacs.d/rmbd/hlint.el
@@ -1,10 +1,7 @@
 (defun hlint ()
   "`hlint' the current file"
   (interactive)
-  (shell-command 
-   (format "hlint %s"
-	   (shell-quote-argument (buffer-file-name))
-	   )
-   )
+  (shell-command
+   (format "hlint %s" (shell-quote-argument (buffer-file-name))))
   (revert-buffer t t t)
   )
