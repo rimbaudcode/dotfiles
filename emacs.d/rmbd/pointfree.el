9e333fde9ecb28c7e2cdbdbf22c6c0303bbdc1f2
art rimbaud <rimbaudcode@gmail.com>
2016-10-24 23:55:44 +0200

    pointfree displays result in minibuffer instead of bindly replacing code

--- emacs.d/rmbd/pointfree.el
+++ emacs.d/rmbd/pointfree.el
@@ -1,18 +1,27 @@
 (defun pointfree ()
-  "run `pointfree` with the current selected region"
+  "run `pointfree` against the current selected region"
   (interactive)
-  (shell-command-on-region
-   ;; beginning and end of selected region
-   (region-beginning)
-   (region-end)
-   ;; command and parameters
-   "pointfree --stdin"
-   ;; output buffer
-   (current-buffer)
-   ;; replace?
-   t
-   ;; name of the error buffer
-   "*Tidy Error Buffer*"
-   ;; show error buffer?
-   t)
+  (when (region-active-p)
+    (shell-command-on-region
+     ;; beginning and end of selected region
+     (region-beginning)
+     (region-end)
+     ;; command and parameters
+     "pointfree --stdin"
+     ;; output buffer
+     (message "%s" (let ((string (buffer-substring
+                                  (region-beginning)
+                                  (region-end))))
+                     (with-temp-buffer
+                       (insert string)
+                       (buffer-substring-no-properties
+                        (point-min)
+                        (point-max))))))
+    ;; replace?
+    t
+    ;; name of the error buffer
+    "*Tidy Error Buffer*"
+    ;; show error buffer?
+    t
+    )
   )
