934056c693883251ee8ac34cd554f1b42ad9bd75
art rimbaud <rimbaudcode@gmail.com>
2016-10-24 23:56:27 +0200

    code refactored

--- emacs.d/rmbd/stylish-haskell.el
+++ emacs.d/rmbd/stylish-haskell.el
@@ -1,10 +1,7 @@
 (defun stylish-haskell ()
   "`stylish-haskell' on the current file"
   (interactive)
-  (shell-command 
-   (format "stylish-haskell -i %s"
-	   (shell-quote-argument (buffer-file-name))
-	   )
-   )
+  (shell-command
+   (format "stylish-haskell -i %s" (shell-quote-argument (buffer-file-name))))
   (revert-buffer t t t)
   )
