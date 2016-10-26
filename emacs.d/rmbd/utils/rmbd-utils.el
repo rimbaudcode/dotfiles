5e4bced84ce60e9556a4f8474b3868bf6d63bb6b
art rimbaud <rimbaudcode@gmail.com>
2016-10-26 12:40:54 +0200

    convert-md-to-docx added

--- emacs.d/rmbd/utils/rmbd-utils.el
+++ emacs.d/rmbd/utils/rmbd-utils.el
@@ -11,12 +11,24 @@
   )
 
 (defun convert-md-to-pdf ()
-  "Convert the current buffer file into PDF."
+  "Convert the current buffer file into `PDF'."
   (interactive)
   (shell-command
    (format "pandoc %s -o %s"
            (shell-quote-argument (buffer-file-name))
            (shell-quote-argument (concat (file-name-base (buffer-file-name)) ".pdf"))
+           )
+   )
+  (revert-buffer t t t)
+  )
+
+(defun convert-md-to-docx ()
+  "Convert the current buffer file into `.docx'."
+  (interactive)
+  (shell-command
+   (format "pandoc %s -o %s"
+           (shell-quote-argument (buffer-file-name))
+           (shell-quote-argument (concat (file-name-base (buffer-file-name)) ".docx"))
            )
    )
   (revert-buffer t t t)
