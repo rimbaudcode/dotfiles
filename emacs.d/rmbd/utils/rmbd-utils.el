c388d1647447edc1409e6ca036e5abbf6d9f81af
art rimbaud <rimbaudcode@gmail.com>
2016-10-26 13:25:44 +0200

    pandoc current buffer to pdf or docx functions added

--- emacs.d/rmbd/utils/rmbd-utils.el
+++ emacs.d/rmbd/utils/rmbd-utils.el
@@ -10,27 +10,41 @@
   (shell-command (buffer-substring-no-properties start end))
   )
 
-(defun convert-md-to-pdf ()
-  "Convert the current buffer file into `PDF'."
+(defun replace-current-buffer-file-extension-to (ext)
+  "Replace the extension of the current buffer file to `.EXT'."
+  (interactive)
+  (concat (file-name-base (buffer-file-name)) ext)
+  )
+
+(defun pandoc-infile-to-outfile (infile outfile)
+  "Convert the type of an INFILE into an OUTFILE using `pandoc'."
   (interactive)
   (shell-command
    (format "pandoc %s -o %s"
-           (shell-quote-argument (buffer-file-name))
-           (shell-quote-argument (concat (file-name-base (buffer-file-name)) ".pdf"))
+           (shell-quote-argument infile)
+           (shell-quote-argument outfile)
            )
    )
   (revert-buffer t t t)
   )
 
-(defun convert-md-to-docx ()
-  "Convert the current buffer file into `.docx'."
+(defun pandoc-md-to-pdf ()
+  "Convert the current MD buffer file into `PDF'."
   (interactive)
-  (shell-command
-   (format "pandoc %s -o %s"
-           (shell-quote-argument (buffer-file-name))
-           (shell-quote-argument (concat (file-name-base (buffer-file-name)) ".docx"))
-           )
-   )
+  (pandoc-infile-to-outfile
+    (buffer-file-name)
+    (replace-current-buffer-file-extension-to ".pdf")
+    )
+  (revert-buffer t t t)
+  )
+
+(defun pandoc-md-to-docx ()
+  "Convert the current MD buffer file into `docx'."
+  (interactive)
+  (pandoc-infile-to-outfile
+    (buffer-file-name)
+    (replace-current-buffer-file-extension-to ".docx")
+    )
   (revert-buffer t t t)
   )
 
