b8505501c1e605ab4798f4c5221540b9c12e1ac9
art rimbaud <rimbaudcode@gmail.com>
2016-10-27 19:04:16 +0200

    magit-gitflow added

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -59,7 +59,7 @@
      ("melpa" . "http://melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-    (darkroom csv-mode flycheck-haskell smex yaml-mode magit yasnippet swift3-mode markdown-mode auto-package-update multiple-cursors intero iedit hi2 haskell-tab-indent fill-column-indicator diminish auto-complete)))
+    (magit-gitflow darkroom csv-mode flycheck-haskell smex yaml-mode magit yasnippet swift3-mode markdown-mode auto-package-update multiple-cursors intero iedit hi2 haskell-tab-indent fill-column-indicator diminish auto-complete)))
  '(proof-splash-enable nil)
  '(server-mode nil)
  '(show-paren-mode t)
