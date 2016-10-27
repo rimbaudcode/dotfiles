5925e2fb546205f0e593470c8082d536317cc1fc
art rimbaud <rimbaudcode@gmail.com>
2016-10-27 16:17:01 +0200

    csv-mode and darkroom installed

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -59,7 +59,7 @@
      ("melpa" . "http://melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-    (flycheck-haskell smex yaml-mode magit yasnippet swift3-mode markdown-mode auto-package-update multiple-cursors intero iedit hi2 haskell-tab-indent fill-column-indicator diminish auto-complete)))
+    (darkroom csv-mode flycheck-haskell smex yaml-mode magit yasnippet swift3-mode markdown-mode auto-package-update multiple-cursors intero iedit hi2 haskell-tab-indent fill-column-indicator diminish auto-complete)))
  '(proof-splash-enable nil)
  '(server-mode nil)
  '(show-paren-mode t)
