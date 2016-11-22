3f8b978f672e9a747ab74c7955d6545ce52e9c6c
art rimbaud <rimbaudcode@gmail.com>
2016-11-22 10:38:29 +0100

    emacs updated

--- emacs.d/elpa/dash-20161121.55/dash-autoloads.el
+++ emacs.d/elpa/dash-20161121.55/dash-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("dash.el") (22577 43857 0 0))
+;;;### (autoloads nil nil ("dash.el") (22580 4426 0 0))
 
 ;;;***
 
