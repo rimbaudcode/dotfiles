b1f714cf075947674eeab44fc0b8c4440ab35d05
art rimbaud <rimbaudcode@gmail.com>
2016-11-27 13:00:07 +0100

    emacs: aggresive-indent-mode installed and in use

---
+++ emacs.d/elpa/crux-20161005.634/crux-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "crux" "20161005.634" "A Collection of Ridiculously Useful eXtensions" '((seq "1.11")) :url "https://github.com/bbatsov/crux" :keywords '("convenience"))
