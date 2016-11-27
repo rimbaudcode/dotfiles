f0a0f01a19ec74a79cdde4c67a27ad526d027cb0
art rimbaud <rimbaudcode@gmail.com>
2016-11-27 18:50:47 +0100

    emacs: code refactored

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -125,41 +125,9 @@
 ;;
 (require 'darkroom)
 ;;
-;; simpler prompts
-;;
-(fset 'yes-or-no-p 'y-or-n-p)
-;;
-;; multiple-cursors
-;;
-;; active region
-(global-set-key (kbd "C-l") 'mc/edit-lines)
-;;
-;; keywords in buffer
-(global-set-key (kbd "C-d") 'mc/mark-next-like-this)
-;;(global-set-key (kbd "C-r") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-r") 'mc/mark-all-like-this)
-;;
-;; scrolling
-;; NOTE: scrolling has the opposite direction of cursors!
-(global-set-key (kbd "ESC <up>") 'scroll-down-command)
-(global-set-key (kbd "ESC <down>") 'scroll-up-command)
-;;
 (load-file "~/github/dotfiles/emacs.d/rmbd/rmbd-haskell.el")
 (load-file "~/github/dotfiles/emacs.d/rmbd/rmbd-stack.el")
 (load-file "~/github/dotfiles/emacs.d/rmbd/rmbd-utils.el")
-;;
-;; delete-white-space on saving
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-;;
-;; tell dired to use GNU ls (gls) instead of BSD ls
-(setq insert-directory-program (executable-find "gls"))
-;;
-;; use tab to indent or complete
-(setq tab-always-indent 'complete)
-;;
-;; open other windows vertically
-(setq split-height-threshold nil)
-(setq split-width-threshold 80)
 ;;
 (provide 'emacs)
 ;;; init.el ends here
--- emacs.d/rmbd/rmbd-utils.el
+++ emacs.d/rmbd/rmbd-utils.el
@@ -4,6 +4,49 @@
 
 ;;; code:
 
+;; delete-white-space on saving
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+;;
+;; tell dired to use GNU ls (gls) instead of BSD ls
+(setq insert-directory-program (executable-find "gls"))
+;;
+;; use tab to indent or complete
+(setq tab-always-indent 'complete)
+;;
+;; open other windows vertically
+(setq split-height-threshold nil)
+(setq split-width-threshold 80)
+;;
+;; delete-white-space on saving
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+;;
+;; tell dired to use GNU ls (gls) instead of BSD ls
+(setq insert-directory-program (executable-find "gls"))
+;;
+;; use tab to indent or complete
+(setq tab-always-indent 'complete)
+;;
+;; open other windows vertically
+(setq split-height-threshold nil)
+(setq split-width-threshold 80)
+;;
+;; simpler prompts
+(fset 'yes-or-no-p 'y-or-n-p)
+;;
+;; multiple-cursors
+;; active region
+(global-set-key (kbd "C-l") 'mc/edit-lines)
+;;
+;; keywords in buffer
+(global-set-key (kbd "C-d") 'mc/mark-next-like-this)
+;;(global-set-key (kbd "C-r") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-r") 'mc/mark-all-like-this)
+;;
+;; scrolling
+;; NOTE: scrolling has the opposite direction of cursors!
+(global-set-key (kbd "ESC <up>") 'scroll-down-command)
+(global-set-key (kbd "ESC <down>") 'scroll-up-command)
+
 (defun delete-file-and-buffer ()
   "Kill the current buffer and deletes the file it is visiting."
   (interactive)
@@ -111,5 +154,23 @@
   (shell-command
    (format "wiki -short '%s'" (buffer-substring-no-properties start end))))
 
+;; better window splitting
+(defun vsplit-last-buffer ()
+  "Vertically split the last buffer after having opened a new buffer."
+  (interactive)
+  (split-window-vertically)
+  (other-window 1 nil)
+  (switch-to-next-buffer))
+;;
+(defun hsplit-last-buffer ()
+  "Horizontally split the last buffer after having opened a new buffer."
+  (interactive)
+  (split-window-horizontally)
+  (other-window 1 nil)
+  (switch-to-next-buffer))
+;;
+(global-set-key (kbd "C-x 2") 'vsplit-last-buffer)
+(global-set-key (kbd "C-x 3") 'hsplit-last-buffer)
+;;
 (provide 'rmbd-utils.el)
 ;;; rmbd-utils.el ends here
