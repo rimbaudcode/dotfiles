30d9fe405d4ef0252d3f6b94cb5ff54d7868d39a
art rimbaud <rimbaudcode@gmail.com>
2016-11-29 14:57:11 +0100

    refactor: emacs.d/rmbd/rmbd-wiki.el. abstracted code

--- emacs.d/rmbd/rmbd-wiki.el
+++ emacs.d/rmbd/rmbd-wiki.el
@@ -6,29 +6,36 @@
 
 (require 'thingatpt)
 
+(defun wiki-search-current-region (opt start end)
+  "Search the current marked region, using `wiki' passing to it an OPT, from START to END."
+  (shell-command (format "wiki %s %s" opt
+                         (shell-quote-argument
+                          (buffer-substring-no-properties start end)))))
+
 (defun wiki-search-region (start end)
-  "Search the current marked string from START to END using `wiki'."
+  "Search the current marked string from START to END  `wiki'."
   (interactive "r")
-  (shell-command
-   (format "wiki -n '%s'" (buffer-substring-no-properties start end))))
+  (wiki-search-current-region "-n" start end))
 
 (defun wiki-search-region-short (start end)
   "Search the current marked string from START to END using `wiki' short output."
   (interactive "r")
-  (shell-command
-   (format "wiki -short '%s'" (buffer-substring-no-properties start end))))
+  (wiki-search-current-region "-short" start end))
+
+(defun wiki-search-current-word-at-point (opt)
+  "Search the current word under the cursor using `wiki' and passing to it an OPT."
+  (shell-command (format "wiki %s %s" opt
+                         (shell-quote-argument (thing-at-point 'word 'NO-PROPERTIES)))))
 
 (defun wiki-search-word-at-point ()
   "Search the current word under the cursor using `wiki' short output."
   (interactive)
-  (shell-command (format "wiki -n '%s'"
-                         (thing-at-point 'word 'NO-PROPERTIES))))
+  (wiki-search-current-word-at-point "-n"))
 
 (defun wiki-search-word-at-point-short ()
   "Search the current word under the cursor using `wiki' short output."
   (interactive)
-  (shell-command (format "wiki -short '%s'"
-                         (thing-at-point 'word 'NO-PROPERTIES))))
+  (wiki-search-current-word-at-point "-short"))
 
 (provide 'rmbd-wiki)
 ;;; rmbd-wiki.el ends here
