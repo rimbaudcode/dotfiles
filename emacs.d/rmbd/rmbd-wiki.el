af21531588d144905441e04af1cbe77bc1a7e561
art rimbaud <rimbaudcode@gmail.com>
2016-11-30 02:11:00 +0100

    refactor: emacs.d/rmbd/rmbd-wiki.el: whitespace added

--- emacs.d/rmbd/rmbd-wiki.el
+++ emacs.d/rmbd/rmbd-wiki.el
@@ -8,8 +8,10 @@
 
 (defun wiki-search-current-region (opt start end)
   "Search the current marked region, using `wiki' passing to it an OPT, from START to END."
-  (shell-command (format "wiki %s %s" opt
-                         (shell-quote-argument (buffer-substring-no-properties start end)))))
+  (shell-command
+   (format "wiki %s %s" opt
+           (shell-quote-argument
+            (buffer-substring-no-properties start end)))))
 
 (defun wiki-search-region (start end)
   "Search the current marked string from START to END  `wiki'."
@@ -23,8 +25,10 @@
 
 (defun wiki-search-current-word-at-point (opt)
   "Search the current word under the cursor using `wiki' and passing to it an OPT."
-  (shell-command (format "wiki %s %s" opt
-                         (shell-quote-argument (thing-at-point 'word 'NO-PROPERTIES)))))
+  (shell-command
+   (format "wiki %s %s" opt
+           (shell-quote-argument
+            (thing-at-point 'word 'NO-PROPERTIES)))))
 
 (defun wiki-search-word-at-point ()
   "Search the current word under the cursor using `wiki' short output."
