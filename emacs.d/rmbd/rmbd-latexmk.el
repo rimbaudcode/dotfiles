7d7a415b78e119b216fb75a1420433d9c501f74c
art rimbaud <rimbaudcode@gmail.com>
2016-12-02 15:51:40 +0100

    refactor: emacs.d/rmbd/rmbd-latexmk.el: better handling of vars

--- emacs.d/rmbd/rmbd-latexmk.el
+++ emacs.d/rmbd/rmbd-latexmk.el
@@ -4,17 +4,15 @@
 
 ;;; code:
 
+(require 'rmbd-utils)
 (require 'thingatpt)
-
-
-;; latexmk -xelatex
 
 (defun latexmk-build-current-buffer-file ()
   "Build the current (buffer) file using `latexmk'."
   (interactive)
-  (async-shell-command (format "latexmk -xelatex -f -quiet %s" (shell-quote-argument (buffer-file-name)))
-                       (get-buffer-create "*latexmk build: standard output*")
-                       (get-buffer-create "*latexmk build: standard error*")))
+  (let ((cmd (format "latexmk -xelatex -f -quiet %s" (shell-quote-argument (buffer-file-name))))
+        (temp-buffer    (get-buffer-create "*latexmk build*")))
+    (async-shell-command cmd temp-buffer)))
 
 (defun latexmk-clean-nonessential-files ()
   "Clean nonessential `TeX' files."
@@ -24,15 +22,10 @@
 (defun latexmk-open-pdf ()
   "Open the `latexmk' generated `PDF' using `macOS' `open' utility."
   (interactive)
-  (shell-command
-   (format "open %s"
-           (shell-quote-argument
-            (replace-current-buffer-file-extension-to ".pdf")))))
-
-(defun replace-current-buffer-file-extension-to (ext)
-  "Replace the extension of the current buffer file to `.EXT'."
-  (interactive)
-  (concat (file-name-base (buffer-file-name)) ext))
+  (let ((cmd (format "open %s"
+                     (shell-quote-argument
+                      (replace-current-buffer-file-extension-to ".pdf")))))
+    (shell-command cmd)))
 
 (provide 'rmbd-latexmk)
 ;;; rmbd-latexmk.el ends here
