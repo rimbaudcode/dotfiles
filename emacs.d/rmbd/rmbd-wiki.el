54d7029dd4b192ec70f17c403006b01bf046b255
art rimbaud <rimbaudcode@gmail.com>
2016-12-03 15:43:02 +0100

    refactor: emacs.d/rmbd/rmbd-wiki.el: simplified

--- emacs.d/rmbd/rmbd-wiki.el
+++ emacs.d/rmbd/rmbd-wiki.el
@@ -6,39 +6,33 @@
 
 (require 'thingatpt)
 
-(defun wiki-search-current-region (opt start end)
-  "Search the current marked region, using `wiki' passing to it an OPT, from START to END."
-  (shell-command
-   (format "wiki %s %s" opt
-           (shell-quote-argument
-            (buffer-substring-no-properties start end)))))
-
 (defun wiki-search-region (start end)
   "Search the current marked string from START to END  `wiki'."
   (interactive "r")
-  (wiki-search-current-region "-n" start end))
-
-(defun wiki-search-region-short (start end)
-  "Search the current marked string from START to END using `wiki' short output."
-  (interactive "r")
-  (wiki-search-current-region "-short" start end))
-
-(defun wiki-search-current-word-at-point (opt)
-  "Search the current word under the cursor using `wiki' and passing to it an OPT."
-  (shell-command
-   (format "wiki %s %s" opt
-           (shell-quote-argument
-            (thing-at-point 'word 'NO-PROPERTIES)))))
+  (let ((cmd (format "wiki -n %s"
+                     (shell-quote-argument
+                      (buffer-substring-no-properties start end))))
+        (temp-buffer-name "*wiki*"))
+    (get-buffer-create temp-buffer-name)
+    (shell-command cmd temp-buffer-name)
+    (switch-to-buffer-other-window temp-buffer-name)
+    (goto-char (point-min))
+    (whitespace-cleanup)
+    (special-mode)))
 
 (defun wiki-search-word-at-point ()
-  "Search the current word under the cursor using `wiki' short output."
+  "Search the current word under the cursor using `wiki'."
   (interactive)
-  (wiki-search-current-word-at-point "-n"))
-
-(defun wiki-search-word-at-point-short ()
-  "Search the current word under the cursor using `wiki' short output."
-  (interactive)
-  (wiki-search-current-word-at-point "-short"))
+  (let ((cmd (format "wiki -n %s"
+                     (shell-quote-argument
+                      (thing-at-point 'word 'NO-PROPERTIES))))
+        (temp-buffer-name "*wiki*"))
+    (get-buffer-create temp-buffer-name)
+    (shell-command cmd temp-buffer-name)
+    (switch-to-buffer-other-window temp-buffer-name)
+    (goto-char (point-min))
+    (whitespace-cleanup)
+    (special-mode)))
 
 (provide 'rmbd-wiki)
 ;;; rmbd-wiki.el ends here
