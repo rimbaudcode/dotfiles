9e861c101432c01ced4f2ed381f3537ec23924c8
art rimbaud <rimbaudcode@gmail.com>
2016-12-03 13:50:37 +0100

    emacs: emacs.d/init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -35,7 +35,7 @@
  '(cua-normal-cursor-color "dark gray")
  '(cua-overwrite-cursor-color "dark gray")
  '(cursor-type (quote hollow))
- '(custom-enabled-themes (quote (wombat)))
+ '(custom-enabled-themes (quote (zenburn)))
  '(custom-safe-themes
    (quote
     ("4e753673a37c71b07e3026be75dc6af3efbac5ce335f3707b7d6a110ecb636a3" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "d8f76414f8f2dcb045a37eb155bfaa2e1d17b6573ed43fb1d18b936febc7bbc2" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "08851585c86abcf44bb1232bced2ae13bc9f6323aeda71adfa3791d6e7fea2b6" default)))
