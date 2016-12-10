e25507cb5cd6e06209a8628a0a5607a5bc1eb6fe
art rimbaud <rimbaudcode@gmail.com>
2016-12-10 15:43:11 +0100

    feature: art-view-pdf-named-as-buffer-with added

--- emacs.d/art/art-latexmk.el
+++ emacs.d/art/art-latexmk.el
@@ -4,6 +4,7 @@
 
 ;;; code:
 
+(require 'art-utils)
 (require 'thingatpt)
 
 (defun art-latexmk-build-current-buffer-file ()
@@ -24,6 +25,23 @@
   (interactive)
   (let ((bib-file-path "./doc/references.bib"))
     (shell-command (format "open %s" bib-file-path))))
+
+(defun art-view-pdf-named-as-buffer-with (viewerpath)
+  "Open the `PDF' named as the current buffer using the VIEWERPATH app."
+  (let ((pdf-file (art-replace-buffer-file-extension-to ".pdf"))
+        (cmd "open -a")
+        (viewer-path viewerpath))
+    (shell-command (format "%s %s %s" cmd viewer-path pdf-file))))
+
+(defun art-skim-pdf-named-as-buffer ()
+  "Open the `PDF' named as the current buffer using `Skim'."
+  (interactive)
+  (art-view-pdf-named-as-buffer-with "/Applications/Skim.app/"))
+
+(defun art-preview-pdf-named-as-buffer ()
+  "Open the `PDF' named as the current buffer using `Preview'."
+  (interactive)
+  (art-view-pdf-named-as-buffer-with "/Applications/Preview.app/"))
 
 (provide 'art-latexmk)
 ;;; art-latexmk.el ends here
