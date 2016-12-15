c9ec0baeabf7928f67a4b6c715fc29dc27a300d3
art rimbaud <rimbaudcode@gmail.com>
2016-12-15 19:25:20 +0100

    emacs: updated

--- emacs.d/elpa/which-key-20161215.658/which-key-autoloads.el
+++ emacs.d/elpa/which-key-20161215.658/which-key-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "which-key" "which-key.el" (22602 31310 0 0))
+;;;### (autoloads nil "which-key" "which-key.el" (22610 56417 0 0))
 ;;; Generated autoloads from which-key.el
 
 (defvar which-key-mode nil "\
