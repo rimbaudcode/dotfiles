f7a65b620615311c955f86f135c0fae37315c235
art rimbaud <rimbaudcode@gmail.com>
2016-12-20 12:47:47 +0100

    emacs: updated

--- emacs.d/elpa/crux-20161005.634/crux-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "crux" "20161005.634" "A Collection of Ridiculously Useful eXtensions" '((seq "1.11")) :url "https://github.com/bbatsov/crux" :keywords '("convenience"))
