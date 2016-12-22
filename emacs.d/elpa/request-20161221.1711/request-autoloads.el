bf21d32fc1932f6b5b41c464c53fb93923396cb0
art rimbaud <rimbaudcode@gmail.com>
2016-12-22 15:41:56 +0100

    emacs: updated

--- emacs.d/elpa/request-20161221.1711/request-autoloads.el
+++ emacs.d/elpa/request-20161221.1711/request-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("request.el") (22611 8665 0 0))
+;;;### (autoloads nil nil ("request.el") (22619 58846 0 0))
 
 ;;;***
 
