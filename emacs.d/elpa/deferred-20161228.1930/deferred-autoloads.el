5b302263e1c67ea5672beb3f0b2f9c39e89633c2
art rimbaud <rimbaudcode@gmail.com>
2016-12-29 16:08:59 +0100

    emacs: updated

--- emacs.d/elpa/deferred-20161228.1930/deferred-autoloads.el
+++ emacs.d/elpa/deferred-20161228.1930/deferred-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("deferred.el") (22589 28638 0 0))
+;;;### (autoloads nil nil ("deferred.el") (22629 8891 0 0))
 
 ;;;***
 
