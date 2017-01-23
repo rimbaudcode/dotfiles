58ca437ebe2c485e227ccb3c0ee5125bf1c79aff
art rimbaud <rimbaudcode@gmail.com>
2017-01-23 13:05:08 +0100

    emacs: update

--- emacs.d/elpa/counsel-20170119.859/counsel-autoloads.el
+++ emacs.d/elpa/counsel-20170119.859/counsel-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "counsel" "counsel.el" (22656 42743 0 0))
+;;;### (autoloads nil "counsel" "counsel.el" (22659 13712 0 0))
 ;;; Generated autoloads from counsel.el
 
 (autoload 'counsel-el "counsel" "\
