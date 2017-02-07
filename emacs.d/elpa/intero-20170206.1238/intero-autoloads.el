901a113585b2db64cf84bef75efe7d8ad1462f75
art rimbaud <rimbaudcode@gmail.com>
2017-02-07 11:23:51 +0100

    emacs: update

--- emacs.d/elpa/intero-20170206.1238/intero-autoloads.el
+++ emacs.d/elpa/intero-20170206.1238/intero-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "intero" "intero.el" (22680 20813 0 0))
+;;;### (autoloads nil "intero" "intero.el" (22681 40951 0 0))
 ;;; Generated autoloads from intero.el
 
 (autoload 'intero-mode "intero" "\
