a534bcde91094118c8e1f81a2517cc4f0ff1c187
art rimbaud <rimbaudcode@gmail.com>
2017-02-25 13:04:55 +0100

    emacs: update

--- emacs.d/elpa/helm-20170224.557/helm-pkg.el
+++ emacs.d/elpa/helm-20170224.557/helm-pkg.el
@@ -1,4 +1,4 @@
-(define-package "helm" "20170223.2238" "Helm is an Emacs incremental and narrowing framework"
+(define-package "helm" "20170224.557" "Helm is an Emacs incremental and narrowing framework"
   '((emacs "24.4")
     (async "1.9")
     (popup "0.5.3")
