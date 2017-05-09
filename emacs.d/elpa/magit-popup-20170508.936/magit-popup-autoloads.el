64b71d77cb9f84bb0eb9ec0c817dd021e433e379
art rimbaud <rimbaudcode@gmail.com>
2017-05-09 09:42:01 +0200

    emacs: updated

--- emacs.d/elpa/magit-popup-20170508.936/magit-popup-autoloads.el
+++ emacs.d/elpa/magit-popup-20170508.936/magit-popup-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
-;;;;;;  (22800 10504 0 0))
+;;;;;;  (22801 29305 0 0))
 
 ;;;***
 
