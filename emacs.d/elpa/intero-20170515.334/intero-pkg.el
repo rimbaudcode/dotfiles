55aa785735fe102999b878035c8935c7ed3c1e44
art rimbaud <rimbaudcode@gmail.com>
2017-05-19 11:44:06 +0200

    emacs: updated

--- emacs.d/elpa/intero-20170515.334/intero-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "intero" "20170515.334" "Complete development mode for Haskell" '((flycheck "0.25") (company "0.8") (emacs "24.4") (haskell-mode "13.0")) :commit "5e7e3ea2d68fb95dd23000cb1716df323aaaec81" :url "https://github.com/commercialhaskell/intero" :keywords '("haskell" "tools"))
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -103,7 +103,7 @@
      ("melpa" . "http://melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-    (fsharp-mode emacsql-sqlite io-mode json-reformat rainbow-delimiters rainbow-identifiers ranger racket-mode exec-path-from-shell 0blayout lua-mode flyspell-correct-helm flyspell-lazy helm-xcdoc helm-youtube helm-flyspell helm-google elixir-mode elixir-yasnippets flycheck-elixir tabbar tabbar-ruler flycheck-stack flymake-hlint flymake-shell haskell-snippets osx-dictionary osx-lib osx-trash writeroom-mode wordsmith-mode which-key zenburn-theme counsel swiper ace-window org-bullets popwin deferred define-word google-this google-translate helm-hoogle helm-itunes aggressive-indent crux json-mode gnuplot-mode swift-mode magit-gitflow darkroom csv-mode flycheck-haskell smex yaml-mode magit yasnippet markdown-mode auto-package-update multiple-cursors intero iedit hi2 haskell-tab-indent fill-column-indicator diminish auto-complete)))
+    (skeletor fsharp-mode emacsql-sqlite io-mode json-reformat rainbow-delimiters rainbow-identifiers ranger racket-mode exec-path-from-shell 0blayout lua-mode flyspell-correct-helm flyspell-lazy helm-xcdoc helm-youtube helm-flyspell helm-google elixir-mode elixir-yasnippets flycheck-elixir tabbar tabbar-ruler flycheck-stack flymake-hlint flymake-shell haskell-snippets osx-dictionary osx-lib osx-trash writeroom-mode wordsmith-mode which-key zenburn-theme counsel swiper ace-window org-bullets popwin deferred define-word google-this google-translate helm-hoogle helm-itunes aggressive-indent crux json-mode gnuplot-mode swift-mode magit-gitflow darkroom csv-mode flycheck-haskell smex yaml-mode magit yasnippet markdown-mode auto-package-update multiple-cursors intero iedit hi2 haskell-tab-indent fill-column-indicator diminish auto-complete)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(prog-mode-hook nil)
  '(proof-splash-enable nil)
@@ -114,8 +114,11 @@
  '(server-mode nil)
  '(show-paren-mode t)
  '(show-trailing-whitespace t)
+ '(skeletor-user-organisation "rimbaudcode")
  '(transient-mark-mode t)
  '(uniquify-buffer-name-style (quote post-forward) nil (uniquify))
+ '(user-full-name "Art Rimbaud")
+ '(user-mail-address "rimbaudcode[aT]gmail<DoT>com")
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
    (quote
