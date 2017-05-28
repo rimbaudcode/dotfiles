e7cd5f89a06a87728a239f83fba99fc5a9e4b600
art rimbaud <rimbaudcode@gmail.com>
2017-05-28 13:43:01 +0200

    emacs: updated

---
+++ emacs.d/elpa/which-key-20170527.1448/which-key-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "which-key" "20170527.1448" "Display available keybindings in popup" '((emacs "24.4")) :commit "000d7a68d1ea383d6cca7dcfb88ebcbb3c54190a" :url "https://github.com/justbur/emacs-which-key")
