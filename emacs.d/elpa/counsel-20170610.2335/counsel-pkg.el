30536382487a774e3a10cc20c5f52d07fa11db9a
art rimbaud <rimbaudcode@gmail.com>
2017-06-20 11:15:48 +0200

    update: emacs

--- emacs.d/elpa/counsel-20170610.2335/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel" "20170610.2335" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "752ba81f0db3be0568072b3d9a5db458bb647a3c" :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching"))
--- emacs.d/elpa/intero-20170611.437/intero-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "intero" "20170611.437" "Complete development mode for Haskell" '((flycheck "0.25") (company "0.8") (emacs "24.4") (haskell-mode "13.0")) :commit "70c9c524b3de8dce739df7fe0fdb5128fc8adf11" :url "https://github.com/commercialhaskell/intero" :keywords '("haskell" "tools"))
