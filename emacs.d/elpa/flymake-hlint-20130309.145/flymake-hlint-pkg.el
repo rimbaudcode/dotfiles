4ad1c9ce73ba5ebae9d7f2a61354df0bd5ce4a25
art rimbaud <rimbaudcode@gmail.com>
2017-07-26 22:06:52 -0500

    emacs: updated

--- emacs.d/elpa/flymake-hlint-20130309.145/flymake-hlint-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flymake-hlint" "20130309.145" "A flymake handler for haskell-mode files using hlint" '((flymake-easy "0.1")) :url "https://github.com/purcell/flymake-hlint")
---
+++ emacs.d/elpa/flymake-hlint-20170722.1846/flymake-hlint-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flymake-hlint" "20170722.1846" "A flymake handler for haskell-mode files using hlint" '((flymake-easy "0.1")) :commit "f910736b26784efc9a2fa29503f45c1f1dd0aa38" :url "https://github.com/purcell/flymake-hlint")
