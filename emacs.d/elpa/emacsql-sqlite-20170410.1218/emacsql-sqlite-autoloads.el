28dea1779b71446f9763fa18e2e84ddc66893570
art rimbaud <rimbaudcode@gmail.com>
2017-08-07 10:23:13 -0500

    emacs: updated

--- emacs.d/elpa/emacsql-sqlite-20170410.1218/emacsql-sqlite-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; emacsql-sqlite-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("emacsql-sqlite-pkg.el" "emacsql-sqlite.el"
-;;;;;;  "emacsql-system.el") (22802 63402 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; emacsql-sqlite-autoloads.el ends here
--- emacs.d/elpa/emacsql-sqlite-20170410.1218/emacsql-sqlite-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "emacsql-sqlite" "20170410.1218" "EmacSQL back-end for SQLite"
-  '((emacs "24.3")
-    (cl-generic "0.3")
-    (cl-lib "0.3")
-    (emacsql "2.0.0"))
-  :url "https://github.com/skeeto/emacsql")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
