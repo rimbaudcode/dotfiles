597d50e40f11b4d2818e7b0952b6ad9ff7f22a62
art rimbaud <rimbaudcode@gmail.com>
2017-08-14 08:10:18 -0500

    emacs: updated

---
+++ emacs.d/elpa/markdown-mode-20170812.1201/markdown-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "markdown-mode" "20170812.1201" "Major mode for Markdown-formatted text" '((emacs "24") (cl-lib "0.5")) :commit "c79ab7eac618f54c73ffade336e043dc397ca032" :url "http://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))
