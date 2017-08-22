50bacee66aa087d6b9c184be5f0b119558c1108f
art rimbaud <rimbaudcode@gmail.com>
2017-08-22 09:56:12 -0500

    emacs: updated

--- emacs.d/elpa/git-commit-20170821.1159/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20170821.1159" "Edit Git commit messages" '((emacs "24.4") (dash "20170207.2056") (with-editor "20170111.609")) :commit "8810adb1ff5388c5e4e5a7671b4e207eb49985e2" :url "https://github.com/magit/magit" :keywords '("git" "tools" "vc"))
