42ab98d844a37c67b76290b7fa444441168a97ef
art rimbaud <rimbaudcode@gmail.com>
2017-08-29 21:58:15 -0500

    emacs: updated

---
+++ emacs.d/elpa/fill-column-indicator-20170829.907/fill-column-indicator-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "fill-column-indicator" "20170829.907" "Graphically indicate the fill column" 'nil :commit "a44ac30c4c2b6efdd325604d41a154ff00ae187f" :keywords '("convenience"))
