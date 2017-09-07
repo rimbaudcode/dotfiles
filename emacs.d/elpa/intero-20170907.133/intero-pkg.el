4f10f5f51f369309dee6dcf0d6039f1588adc7f6
art rimbaud <rimbaudcode@gmail.com>
2017-09-07 15:41:25 -0500

    emacs: updated

---
+++ emacs.d/elpa/intero-20170907.133/intero-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "intero" "20170907.133" "Complete development mode for Haskell" '((flycheck "0.25") (company "0.8") (emacs "24.4") (haskell-mode "13.0")) :commit "153abdfb327e023a29ef62eb35098470cb8a4f26" :url "https://github.com/commercialhaskell/intero" :keywords '("haskell" "tools"))
