9c3482bc9d447165e740a70e6fdd28e5a5ea5bf0
art rimbaud <rimbaudcode@gmail.com>
2017-09-26 11:08:31 -0500

    emacs: updated

---
+++ emacs.d/elpa/csharp-mode-20170926.305/csharp-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "csharp-mode" "20170926.305" "C# mode derived mode" 'nil :commit "b20e65fe9b34ecd54a69974288ded42ba05dc0b8" :url "https://github.com/josteink/csharp-mode" :keywords '("c#" "languages" "oop" "mode"))
