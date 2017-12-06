6a2bc147f4a2af31aba7909cacda7e1b74b61532
art rimbaud <rimbaudcode@gmail.com>
2017-12-05 22:59:21 -0500

    emacs: updated

---
+++ emacs.d/elpa/counsel-20171205.1231/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20171205.1231" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "4e3606b9321947520bc594d81c6191d95f3de913" :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching"))
