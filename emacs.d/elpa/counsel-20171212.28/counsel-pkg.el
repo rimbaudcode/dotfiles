923e95fd95370e31d2484aff7164ed15caa03de3
art rimbaud <rimbaudcode@gmail.com>
2017-12-14 09:59:28 -0500

    emacs: updated

---
+++ emacs.d/elpa/counsel-20171212.28/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20171212.28" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "b2175ea5cdfb276fabdc738dcd100e8ed774570d" :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching"))
---
+++ emacs.d/elpa/which-key-20171213.1752/which-key-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "which-key" "20171213.1752" "Display available keybindings in popup" '((emacs "24.4")) :commit "7150aa97954483517aa8395e94c39af9b49516fc" :url "https://github.com/justbur/emacs-which-key")
