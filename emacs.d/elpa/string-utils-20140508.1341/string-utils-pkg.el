439eaee8620b5415a661563725e5dbaf369a2a80
art rimbaud <rimbaudcode@gmail.com>
2018-01-06 19:00:59 -0500

    updated: emacs

--- emacs.d/elpa/string-utils-20140508.1341/string-utils-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "string-utils" "20140508.1341" "String-manipulation utilities" '((list-utils "0.4.2")) :commit "c2232d691617973ecf12a970c6008a161c21da14" :url "http://github.com/rolandwalker/string-utils" :keywords '("extensions"))
