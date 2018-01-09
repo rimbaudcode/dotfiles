5024a4181acc464a8d915fd9095b6d32bb82e118
art rimbaud <rimbaudcode@gmail.com>
2018-01-09 11:52:40 -0500

    emacs: updated

---
+++ emacs.d/elpa/flycheck-swift3-20171231.452/flycheck-swift3-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-swift3" "20171231.452" "Flycheck: Swift support for Apple swift-mode" '((emacs "24.4") (flycheck "26")) :commit "34973cd28ca5e63f8f6328a17fd7b78cc913b93d" :url "https://github.com/GyazSquare/flycheck-swift3" :keywords '("convenience" "languages" "tools"))
