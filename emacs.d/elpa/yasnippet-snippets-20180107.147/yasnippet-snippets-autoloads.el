f0946331421ab708802fe1c22d531a813ecb1da9
art rimbaud <rimbaudcode@gmail.com>
2018-01-15 19:16:03 -0500

    emacs: updated

--- emacs.d/elpa/yasnippet-snippets-20180107.147/yasnippet-snippets-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; yasnippet-snippets-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "yasnippet-snippets" "yasnippet-snippets.el"
-;;;;;;  (23123 58609 63995 467000))
-;;; Generated autoloads from yasnippet-snippets.el
-
-(autoload 'yasnippet-snippets-initialize "yasnippet-snippets" "\
-
-
-\(fn)" nil nil)
-
-(eval-after-load 'yasnippet '(yasnippet-snippets-initialize))
-
-;;;***
-
-;;;### (autoloads nil nil ("yasnippet-snippets-pkg.el") (23123 58609
-;;;;;;  65972 56000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; yasnippet-snippets-autoloads.el ends here
