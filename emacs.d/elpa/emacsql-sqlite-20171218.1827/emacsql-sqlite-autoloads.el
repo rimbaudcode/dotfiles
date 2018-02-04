23a5d241fbd00dd1e65ea0416461493e6b75d4cc
art rimbaud <rimbaudcode@gmail.com>
2018-02-04 18:17:03 -0500

    updated: emacs

--- emacs.d/elpa/emacsql-sqlite-20171218.1827/emacsql-sqlite-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; emacsql-sqlite-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("emacsql-sqlite-pkg.el" "emacsql-sqlite.el"
-;;;;;;  "emacsql-system.el") (23097 61863 250045 513000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; emacsql-sqlite-autoloads.el ends here
---
+++ emacs.d/elpa/intero-20180117.921/intero-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "intero" "20180117.921" "Complete development mode for Haskell" '((flycheck "0.25") (company "0.8") (emacs "24.4") (haskell-mode "13.0")) :commit "b6ef262dee10a92bc31935644e087e83957f6d74" :url "https://github.com/commercialhaskell/intero" :keywords '("haskell" "tools"))
