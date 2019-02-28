c6ed588fab55d07b43d4a27a18ef6d93af566183
art rimbaud <rimbaudcode@gmail.com>
2019-02-27 20:46:42 -0500

    emacs updated

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -41,6 +41,7 @@
  '(c-delete-function (quote mc/mark-all-like-this-dwim))
  '(clean-buffer-list-delay-general 1)
  '(column-number-mode t)
+ '(company-global-modes t)
  '(counsel-mode nil)
  '(cua-mode t nil (cua-base))
  '(cua-normal-cursor-color "dark gray")
@@ -49,7 +50,7 @@
  '(custom-enabled-themes (quote (wombat)))
  '(custom-safe-themes
    (quote
-    ("f5512c02e0a6887e987a816918b7a684d558716262ac7ee2dd0437ab913eaec6" "486759384769d44b22bb46072726c2cfb3ccc3d49342e5af1854784d505ffc01" "4e753673a37c71b07e3026be75dc6af3efbac5ce335f3707b7d6a110ecb636a3" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "d8f76414f8f2dcb045a37eb155bfaa2e1d17b6573ed43fb1d18b936febc7bbc2" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "08851585c86abcf44bb1232bced2ae13bc9f6323aeda71adfa3791d6e7fea2b6" default)))
+    ("8d805143f2c71cfad5207155234089729bb742a1cb67b7f60357fdd952044315" "f5512c02e0a6887e987a816918b7a684d558716262ac7ee2dd0437ab913eaec6" "486759384769d44b22bb46072726c2cfb3ccc3d49342e5af1854784d505ffc01" "4e753673a37c71b07e3026be75dc6af3efbac5ce335f3707b7d6a110ecb636a3" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "d8f76414f8f2dcb045a37eb155bfaa2e1d17b6573ed43fb1d18b936febc7bbc2" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "08851585c86abcf44bb1232bced2ae13bc9f6323aeda71adfa3791d6e7fea2b6" default)))
  '(delete-selection-mode t)
  '(dired-async-mode t)
  '(dired-use-ls-dired nil)
@@ -61,6 +62,7 @@
  '(global-aggressive-indent-mode t)
  '(global-auto-complete-mode t)
  '(global-auto-revert-mode t)
+ '(global-company-mode t)
  '(global-hl-line-mode nil)
  '(global-linum-mode nil)
  '(global-visual-line-mode nil)
@@ -92,6 +94,7 @@
  '(ivy-mode t)
  '(ivy-use-virtual-buffers t)
  '(keyboard-coding-system (quote utf-8-unix))
+ '(lice:default-license "bsd-3-clause")
  '(linum-format "%3d ")
  '(make-backup-files nil)
  '(mc/always-run-for-all t)
@@ -142,7 +145,7 @@
      ("melpa" . "http://melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-    (flymake-racket highlight2clipboard tuareg intero helm-gitignore evil flycheck-swift3 yasnippet-snippets spacegray-theme elpy anaconda-mode purescript-mode wolfram-mode geiser csharp-mode skeletor fsharp-mode emacsql-sqlite io-mode json-reformat rainbow-delimiters rainbow-identifiers racket-mode exec-path-from-shell 0blayout lua-mode flyspell-correct-helm flyspell-lazy helm-xcdoc helm-flyspell helm-google elixir-mode elixir-yasnippets flymake-hlint flymake-shell haskell-snippets osx-dictionary osx-lib osx-trash writeroom-mode wordsmith-mode which-key zenburn-theme counsel swiper org-bullets popwin deferred google-this google-translate helm-hoogle aggressive-indent crux json-mode gnuplot-mode swift-mode magit-gitflow darkroom csv-mode smex yaml-mode magit yasnippet markdown-mode auto-package-update multiple-cursors hi2 haskell-tab-indent fill-column-indicator diminish auto-complete)))
+    (matlab-mode define-word quack lice flymake-racket highlight2clipboard tuareg intero helm-gitignore evil flycheck-swift3 yasnippet-snippets spacegray-theme elpy anaconda-mode purescript-mode wolfram-mode geiser csharp-mode skeletor fsharp-mode emacsql-sqlite io-mode json-reformat rainbow-delimiters rainbow-identifiers racket-mode exec-path-from-shell 0blayout lua-mode flyspell-correct-helm flyspell-lazy helm-xcdoc helm-flyspell helm-google elixir-mode elixir-yasnippets flymake-hlint flymake-shell haskell-snippets osx-dictionary osx-lib osx-trash writeroom-mode wordsmith-mode which-key zenburn-theme counsel swiper org-bullets popwin deferred google-this google-translate helm-hoogle aggressive-indent crux json-mode gnuplot-mode swift-mode magit-gitflow darkroom csv-mode smex yaml-mode magit yasnippet markdown-mode auto-package-update multiple-cursors hi2 haskell-tab-indent fill-column-indicator diminish auto-complete)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(prog-mode-hook (quote (hilite-todos)))
  '(proof-splash-enable nil)
@@ -315,11 +318,34 @@
 ;;
 (add-hook 'latex-mode-hook 'art-writing-mood)
 ;;
-;; associate Mathematica .wls files to wolframmode
-(add-to-list 'auto-mode-alist '("\\.wls\\'" . wolfram-mode))
-;;
 ;; associate Tex .tex files to latex-mode
 (add-to-list 'auto-mode-alist '("\\.tex\\'" . latex-mode))
+;;
+;; mathematica
+;;
+;; associate Mathematica .wls files to wolfram-mode
+;;
+(add-to-list 'auto-mode-alist '("\\.wls\\'" . wolfram-mode))
+;;
+;; racket
+;;
+;; associate Racket .rkt files to racket-mode
+(add-to-list 'auto-mode-alist '("\\.rkt\\'" . racket-mode))
+;;
+;; hooks
+(add-hook 'racket-mode-hook 'rainbow-delimiters-mode)
+;;(add-hook 'racket-mode-hook 'auto-complete-mode)
+;;
+;;
+;; scheme
+;;
+;; associate scheme .ss files to scheme-mode
+(add-to-list 'auto-mode-alist '("\\.ss\\'" . scheme-mode))
+;;
+;;
+;; octave
+;; associate scheme .ss files to octave-mode
+(add-to-list 'auto-mode-alist '("\\.m\\'" . octave-mode))
 ;;
 ;; added at the end of file by Emacs customization
 (put 'upcase-region 'disabled nil)
@@ -341,8 +367,8 @@
 ;;
 ;; flymake-racket mode
 ;;
-(add-hook 'scheme-mode-hook #'flymake-racket-add-hook)
-(add-hook 'racket-mode-hook #'flymake-racket-add-hook)
+;;(add-hook 'scheme-mode-hook #'flymake-racket-add-hook)
+;;(add-hook 'racket-mode-hook #'flymake-racket-add-hook)
 ;;
 ;;
 (provide 'emacs)
